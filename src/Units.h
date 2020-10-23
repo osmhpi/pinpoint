0444cdc546ca2e6fb53f616bd9d46bb5a0b439a7
Sven Koehler <sven.koehler@hpi.de>
2020-10-23 17:37:34 +0200

    Include wrapper around units, add energy-delayed product

---
+++ src/Units.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <chrono>
+
+#define DISABLE_PREDEFINED_UNITS
+#define ENABLE_PREDEFINED_POWER_UNITS
+#define ENABLE_PREDEFINED_ENERGY_UNITS
+#define ENABLE_PREDEFINED_TIME_UNITS
+#include "3rdparty/units.h"
+
+namespace units {
+
+UNIT_ADD_WITH_METRIC_PREFIXES(edp, joule_second, joule_seconds, Js, unit<std::ratio<1>, units::compound_unit<units::energy::joules, units::time::seconds>>);
+
+}
+
+units::time::second_t as_unit_seconds(const std::chrono::duration<double> & std_seconds)
+{
+	return units::time::second_t(std_seconds.count());
+}
+
+
+// Just required for proper formated abbrevations in io
+
+using namespace units::power;
+using namespace units::energy;
+using namespace units::time;
+using namespace units::edp;
