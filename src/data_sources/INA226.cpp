d68eb337d944fca99a8763fedf7f066b99c87eff
leogeier <leonard.geier@hotmail.com>
2020-11-16 00:12:18 +0000

    Fix whitespace

--- src/data_sources/INA226.cpp
+++ src/data_sources/INA226.cpp
@@ -2,109 +2,109 @@
 
 #include "Registry.h"
 
-#include <fstream>
 #include <map>
 
 std::map<std::string, std::string> counterNameToFileName;
 
 #if defined(__linux__)
 
+#include <fstream>
 #include <dirent.h>
 
 struct INA226Detail
 {
-  std::ifstream ifstrm;
+	std::ifstream ifstrm;
 };
 
 std::string searchPath = "/sys/class/hwmon/";
 
 std::vector<std::string> INA226::detectAvailableCounters()
 {
-  std::vector<std::string> counters;
+	std::vector<std::string> counters;
 
-  std::shared_ptr<DIR> dir(opendir(searchPath.c_str()), [](DIR *dir) {if (dir) closedir(dir);});
-  if (!dir) return counters;
+	std::shared_ptr<DIR> dir(opendir(searchPath.c_str()), [](DIR *dir) {if (dir) closedir(dir);});
+	if (!dir) return counters;
 
-  struct dirent *entry;
-  while ((entry = readdir(dir.get()))) {
-    if (entry->d_type != DT_LNK) continue;
+	struct dirent *entry;
+	while ((entry = readdir(dir.get()))) {
+		if (entry->d_type != DT_LNK) continue;
 
-    auto devicePath = searchPath + entry->d_name + "/";
-    std::ifstream nameFile(devicePath + "name");
+		auto devicePath = searchPath + entry->d_name + "/";
+		std::ifstream nameFile(devicePath + "name");
 
-    std::string deviceName;
-    nameFile >> deviceName;
+		std::string deviceName;
+		nameFile >> deviceName;
 
-    if (deviceName == "ina226") {
-      std::string counterName = "power-";
-      counterName += entry->d_name;
+		if (deviceName == "ina226") {
+			std::string counterName = "power-";
+			counterName += entry->d_name;
 
-      counters.push_back(counterName);
-      counterNameToFileName.insert({counterName, devicePath + "power1_input"});
-    }
-  }
+			counters.push_back(counterName);
+			counterNameToFileName.insert({counterName, devicePath + "power1_input"});
+		}
+	}
 
-  return counters;
+	return counters;
 }
 
 PowerSample INA226::read() {
-  m_detail->ifstrm.seekg(std::ios_base::beg);
-  int val;
-  m_detail->ifstrm >> val;
+	m_detail->ifstrm.seekg(std::ios_base::beg);
+	int val;
+	m_detail->ifstrm >> val;
 
-  return PowerSample(units::power::microwatt_t(val));
+	return PowerSample(units::power::microwatt_t(val));
 }
 
 INA226::INA226(const std::string &filename) :
-  PowerDataSource(),
-  m_detail(new INA226Detail)
+	PowerDataSource(),
+	m_detail(new INA226Detail)
 {
-  m_detail->ifstrm = std::ifstream(filename);
+	m_detail->ifstrm = std::ifstream(filename);
 
-  if (!m_detail->ifstrm.is_open()) {
-    throw std::runtime_error("Cannot open INA226 file " + filename);
-  }
+	if (!m_detail->ifstrm.is_open()) {
+		throw std::runtime_error("Cannot open INA226 file " + filename);
+	}
 }
 
 #else
 
 struct INA226Detail
 {
-  ;
+	;
 };
 
 std::vector<std::string> INA226::detectAvailableCounters() {
-  return std::vector<std::string>();
+	return std::vector<std::string>();
 }
 
 INA226::INA226(const std::string &filename) :
-  m_detail(new INA226Detail)
+	m_detail(new INA226Detail)
 {
 
 }
 
 PowerSample INA226::read()
 {
-  return PowerSample();
+	return PowerSample();
 }
 
 #endif
 
 PowerDataSourcePtr INA226::openCounter(const std::string &counterName)
 {
-  return PowerDataSourcePtr(new INA226(counterNameToFileName.at(counterName)));
+	return PowerDataSourcePtr(new INA226(counterNameToFileName.at(counterName)));
 }
 
 Aliases INA226::possibleAliases()
 {
-  if (counterNameToFileName.size() >= 1) {
-    return {{"IN", counterNameToFileName.cbegin()->first}};
-  }
+	if (counterNameToFileName.size() >= 1) {
+		return {{"IN", counterNameToFileName.cbegin()->first}};
+	}
 
-  return {};
+	return {};
 }
 
 INA226::~INA226()
 {
-  delete m_detail;
+	delete m_detail;
 }
--- src/data_sources/INA226.h
+++ src/data_sources/INA226.h
@@ -2,7 +2,7 @@
 
 #include <PowerDataSource.h>
 
-// This class likely be used for all INA2xx chips with minor adjustments
+// This class can likely be used for all INA2xx chips with minor adjustments
 // See https://www.kernel.org/doc/html/latest/hwmon/ina2xx.html
 
 struct INA226Detail;
@@ -10,21 +10,21 @@ struct INA226Detail;
 class INA226: public PowerDataSource
 {
 public:
-  static std::string sourceName()
-  {
-    return "ina226";
-  }
+	static std::string sourceName()
+	{
+		return "ina226";
+	}
 
-  static std::vector<std::string> detectAvailableCounters();
-  static PowerDataSourcePtr openCounter(const std::string & counterName);
-  static Aliases possibleAliases();
+	static std::vector<std::string> detectAvailableCounters();
+	static PowerDataSourcePtr openCounter(const std::string & counterName);
+	static Aliases possibleAliases();
 
-  virtual PowerSample read() override;
+	virtual PowerSample read() override;
 
-  virtual ~INA226();
+	virtual ~INA226();
 
 private:
-  INA226(const std::string &filename);
+	INA226(const std::string &filename);
 
-  struct INA226Detail *m_detail;
+	struct INA226Detail *m_detail;
 };
